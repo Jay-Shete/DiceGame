3cfb181904aa0b3101fa6246cb7c5d15efbf1184
Jay-Shete <jaysht60@gmail.com>
2022-04-30 01:58:47 -0700

    added h6 to hit refresh

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var randomNumber1 = Math.ceil(Math.random() * 6);
 
 var randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
 
-document.querySelector(".img1").setAttribute("src", randomDiceImage1)
+document.querySelector("img")[0].setAttribute("src", randomDiceImage1)
 
 var randomNumber2 = Math.ceil(Math.random() * 6);
 
 var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
 
-document.querySelector(".img2").setAttribute("src", randomDiceImage2)
+document.querySelector("img")[1].setAttribute("src", randomDiceImage2)
 
 if(randomNumber1 === randomNumber2){
   document.querySelector("h1").innerHTML = "Draw";
